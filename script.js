d0f5c6fb68a75dff24b7226df28960788fee6673
Harshal Ghuge <hxrsh369@gmail.com>
2026-04-28 08:34:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const divs = document.querySelectorAll(".container div");
+
+let draggedId = null;
+
+divs.forEach(div => {
+  div.addEventListener("dragstart", (e) => {
+    draggedId = e.target.id;
+  });
+
+  div.addEventListener("dragover", (e) => {
+    e.preventDefault(); // required
+  });
+
+  div.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    const targetId = e.target.id;
+
+    if (draggedId !== targetId) {
+      const draggedEl = document.getElementById(draggedId);
+      const targetEl = document.getElementById(targetId);
+
+     
+      const temp = draggedEl.style.backgroundImage;
+      draggedEl.style.backgroundImage = targetEl.style.backgroundImage;
+      targetEl.style.backgroundImage = temp;
+    }
+  });
+});
